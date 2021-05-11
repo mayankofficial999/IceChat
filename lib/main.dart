30678e3426741df42c55576a91c3372cb0276e13
Mayank Sharma <mayankofficial999@gmail.com>
2021-05-11 23:35:14 +0530

    minor_fix

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
+//import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -61,6 +62,8 @@ class _LoginScreenState extends State<LoginScreen>with TickerProviderStateMixin 
   bool loading=false;
   File _image;
   final picker = ImagePicker();
+  //final prefs = await SharedPreferences.getInstance();
+  final myController = TextEditingController();
   Future getImage() async {
     final pickedFile = await picker.getImage(source: ImageSource.gallery);
     setState(() {
@@ -84,10 +87,19 @@ class _LoginScreenState extends State<LoginScreen>with TickerProviderStateMixin 
     } else {
       setState(() {
       signedIn=true;
+      myController.text=FirebaseAuth.instance.currentUser.displayName;
       });
       print('User signed in');
     }
   });
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is removed from the
+    // widget tree.
+    myController.dispose();
+    super.dispose();
   }
 
   @override
@@ -101,33 +113,41 @@ class _LoginScreenState extends State<LoginScreen>with TickerProviderStateMixin 
         ),
       body: Container(
         constraints: BoxConstraints.expand(),
+        //Add Background Image
         decoration: BoxDecoration(
         image: DecorationImage(
           image: AssetImage('assets/background/theme.jpg'),
           fit: BoxFit.cover)
         ),
-        child: loading? Center(child:SizedBox(child:CircularProgressIndicator(
+        //Login/Update Screen
+        child: 
+        //Show loading bar when loading otherwise login/profile_update Screen
+        loading? 
+        Center(child:SizedBox(child:CircularProgressIndicator(
           backgroundColor: Colors.white,
           valueColor: AlwaysStoppedAnimation(Colors.lightBlue),
           strokeWidth: 5,
         ),height: 100,width: 100,),)
-        :Column(children:
+        :
+        Column(children:
         [
+          //Half filled : Login Screen, Full filled : Update Profile Screen
           LinearProgressIndicator(
             value: signedIn? 1.0:0.5,
             valueColor: AlwaysStoppedAnimation(Colors.orangeAccent),
           ),
+          //Move to profile_update after login
           signedIn ? profileUpdate():loginScreen()
         ]
         ),
         ),
       );
   }
-  Widget loginScreen()
-  {
+  Widget loginScreen(){
     return Column(
           children:
           [
+            //App Name
             Container(child:
               Text('IceChat',
                 style: GoogleFonts.concertOne(fontSize: 40,
@@ -138,35 +158,49 @@ class _LoginScreenState extends State<LoginScreen>with TickerProviderStateMixin 
               ),
             margin: EdgeInsets.only(top: 190,bottom: 140),
             ),
+            //Google Sign-in Button
             SignInButton(
               Buttons.Google,
               text: "Sign in with Google",
-              onPressed: () async {setState((){loading=true;});chatUser= await signInWithGoogle();setState((){loading=false;});},
+              onPressed: () async {
+                setState((){loading=true;});
+                chatUser= await signInWithGoogle();
+                // prefs.setString('DP', chatUser.photoURL);
+                // prefs.setString('Name', chatUser.displayName);
+                // prefs.setString('Mail', chatUser.email);
+                // prefs.setString('UID', chatUser.uid);
+                setState((){loading=false;});
+                },
             )
           ]
           );
           
   }
-  Widget profileUpdate()
-  {
+
+  Widget profileUpdate(){
     return Column(children: 
     [
       SizedBox(height: 150,),
+      //Profile Picture
       SizedBox(child:
         ClipOval(child:
+        //Display Changed DP or default fetched dp or default profile
         _image == null
-            ? 
+        ? 
         CachedNetworkImage(
-          imageUrl: signedIn ? chatUser.photoURL:"https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg",
+          imageUrl: signedIn ? FirebaseAuth.instance.currentUser.photoURL:"https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg",
           placeholder: (context, url) => new CircularProgressIndicator(),
           errorWidget: (context, url, error) => new Icon(Icons.error),
           )
-          : Image.file(_image,fit: BoxFit.fill,),
+        : 
+        Image.file(_image,fit: BoxFit.fill,),
         ),
         height: 90,
         width: 90,
       ),
+      //Button to change profile picture
       TextButton(onPressed: (){getImage();}, child: Text('Change')),
+      //Modify User Name
       SizedBox(child:
        TextFormField(
          decoration: const InputDecoration(
@@ -174,7 +208,7 @@ class _LoginScreenState extends State<LoginScreen>with TickerProviderStateMixin 
               hintText: 'Enter Full Name',
               labelText: 'Name',
               ),
-              initialValue: signedIn? chatUser.displayName:'',
+              controller: myController,
             ),
           width: 300,
           ),
